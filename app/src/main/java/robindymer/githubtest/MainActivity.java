d56e329813eee2df453b36cb1c898e5b379cad2d
robindymer <robinlidman@hotmail.com>
2017-06-22 20:48:48 +0200

    Initial commit

---
+++ app/src/main/java/robindymer/githubtest/MainActivity.java
@@ -0,0 +1,13 @@
+package robindymer.githubtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
